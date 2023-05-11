928610cb2b1b31575f906b6c158ea37f93e6c6e7
FacuLara <132574660+FacuLara@users.noreply.github.com>
2023-05-11 15:25:24 -0300

    mirenlo antes de subir al main

--- test.hs
+++ test.hs
@@ -23,54 +23,45 @@ tests = test [
 
     " lesGustanLasMismasPublicaciones" ~: testlesGustanLasMismasPublicaciones,
 
-    -- " tieneUnSeguidorFiel 1" ~: tieneUnSeguidorFiel redA usuario1 ~?= True,
+    " tieneUnSeguidorFiel" ~: testTieneUnSeguidorFiel,
 
     " existeSecuenciaDeAmigos" ~: testExisteSecuenciaDeAmigos
  ]
 
 
 testNombresDeUsuarios = test [
-    nombresDeUsuarios redUno ~?= ["Cami","Agus","Facu","Martu","Pedro","Messi"],
-    nombresDeUsuarios redDos ~?= ["Cami","Agus","Facu"]
+    nombresDeUsuarios redUno ~?= ["Cami","Agus","Facu","Martu","Pedro","Ramon","Jorge","Silvina","Agustin","Colo"],
+    nombresDeUsuarios redTres ~?= []
     ]
 
 testAmigosDe = test [
-    amigosDe redUno usuario1 ~?= [(2, "Agus"), (3, "Facu"), (4, "Martu")],
-    amigosDe redUno usuario2 ~?= [(1, "Cami"), (3, "Facu"),(4, "Martu"), (5, "Pedro")],
-    amigosDe redUno usuario3 ~?= [(1,"Cami"),(2,"Agus"),(4,"Martu")], 
-    amigosDe redUno usuario5 ~?= [(2,"Agus")]
+    amigosDe redUno usuario2 ~?= [(1,"Cami"),(3,"Facu"),(4,"Martu"),(5,"Pedro"),(6,"Ramon"),(7,"Jorge"),(8,"Silvina"),(8,"Silvina"),(10,"Colo"),(11,"Martin")],
+    amigosDe redDos usuario7 ~?= []
     ]
 
 testcantidadDeAmigos = test [
     cantidadDeAmigos redUno usuario1 ~?= 3,
-    cantidadDeAmigos redUno usuario2 ~?= 4,
-    cantidadDeAmigos redUno usuario5 ~?= 1,
-    cantidadDeAmigos redDos usuario3 ~?= 2
+    cantidadDeAmigos redDos usuario7 ~?= 0
     ]
 
 testusuarioConMasAmigos = test [
     usuarioConMasAmigos redUno ~?= usuario2,
-    usuarioConMasAmigos redDos ~?= usuario1 
+    usuarioConMasAmigos redDos ~?= usuario1 -- acá me podría devolver usuario1, usuario2 o usuario3 por eso el caso
     ]
 
 testestaRobertoCarlos = test [
-    estaRobertoCarlos redUno ~?= False,
+    estaRobertoCarlos redUno ~?= True,
     estaRobertoCarlos redDos ~?= False
     ]
 
 testpublicacionesDe = test [
-    publicacionesDe redUno usuario2 ~?= [((2,"Agus"),"Hello World",[(4,"Martu")]),((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(2,"Agus")])],
-    publicacionesDe redDos usuario1 ~?= [((1,"Cami"),"Este es mi primer post",[(2,"Agus"),(4,"Martu"),(5,"Pedro")]),((1,"Cami"),"No s\233 que postear",[(4,"Martu")])],
-    publicacionesDe redDos usuario2 ~?= [((2,"Agus"),"Hello World",[(4,"Martu")]),((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(2,"Agus")])],
-    publicacionesDe redDos usuario3 ~?= [((3,"Facu"),"No",[]),((3,"Facu"),"Si",[(2,"Agus")])]
+    publicacionesDe redUno usuario2 ~?= [((2,"Agus"),"Hello World",[(4,"Martu")]),((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(4,"Martu"),(5,"Pedro"),(6,"Ramon"),(7,"Jorge"),(8,"Silvina"),(9,"Agustin"),(10,"Colo"),(11,"Martin")])],
+    publicacionesDe redDos usuario7 ~?= [] 
     ]
 
 testpublicacionesQueLeGustanA = test [
-    publicacionesQueLeGustanA redUno usuario3 ~?= [((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(2,"Agus")]),((4,"Martu"),"Just kidding, i am Mariela",[(1,"Cami"),(3,"Facu")])],
-    publicacionesQueLeGustanA redUno usuario5 ~?= [((1,"Cami"),"Este es mi primer post",[(2,"Agus"),(4,"Martu"),(5,"Pedro")]),((1,"Cami"),"Trabajo de haskell",[(2,"Agus"),(5,"Pedro")]),((3,"Facu"),"Inserte frase motivacional",
-                                                  [(1,"Cami"),(5,"Pedro")]),((5,"Pedro"),"Todo bien",[(2,"Agus"),(5,"Pedro")])],
-    publicacionesQueLeGustanA redDos usuario1 ~?= [((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(2,"Agus")])],
-    publicacionesQueLeGustanA redDos usuario3 ~?= [((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(2,"Agus")])]
+    publicacionesQueLeGustanA redUno usuario7 ~?= [((2,"Agus"),"Good Bye World",[(1,"Cami"),(3,"Facu"),(4,"Martu"),(5,"Pedro"),(6,"Ramon"),(7,"Jorge"),(8,"Silvina"),(9,"Agustin"),(10,"Colo"),(11,"Martin")])],
+    publicacionesQueLeGustanA redDos usuario7 ~=? []
     ]
 
 testlesGustanLasMismasPublicaciones = test [
@@ -78,16 +69,17 @@ testlesGustanLasMismasPublicaciones = test [
     lesGustanLasMismasPublicaciones redDos usuario1 usuario2 ~?= True
     ]
 
+testTieneUnSeguidorFiel = test [
+    tieneUnSeguidorFiel redUno usuario2 ~?= True,
+    tieneUnSeguidorFiel redUno usuario5 ~?= False
+    ]
+
+
 testExisteSecuenciaDeAmigos = test [
-    existeSecuenciaDeAmigos redUno usuario1 usuario4 ~?= True,
-    existeSecuenciaDeAmigos redDos usuario2 usuario3 ~?= True,
-    existeSecuenciaDeAmigos redUno usuario1 usuario5 ~?= True,
-    existeSecuenciaDeAmigos redDos usuario3 usuario1 ~?= True,
-    existeSecuenciaDeAmigos redUno usuario4 usuario1 ~?= True,
-    existeSecuenciaDeAmigos redDos usuario3 usuario2 ~?= True,
-    existeSecuenciaDeAmigos redUno usuario5 usuario1 ~?= True,
-    existeSecuenciaDeAmigos redDos usuario1 usuario3 ~?= True,
-    existeSecuenciaDeAmigos redUno usuario1 usuario6 ~?= False
+    existeSecuenciaDeAmigos redUno usuario1 usuario4 ~?= True, --relacionados directamente
+    existeSecuenciaDeAmigos redUno usuario1 usuario5 ~?= True, --relacionados por amigos
+    existeSecuenciaDeAmigos redUno usuario5 usuario1 ~?= True, --mismo que "relacionados directamente" pero con los usuarios en orden indistinto
+    existeSecuenciaDeAmigos redDos usuario1 usuario7 ~?= False --usuario7 no tiene amigos/relaciones, tiene que dar False
     ]
 
 -- Datos:
@@ -96,7 +88,12 @@ usuario2 = (2, "Agus")
 usuario3 = (3, "Facu")
 usuario4 = (4, "Martu")
 usuario5 = (5, "Pedro")
-usuario6 = (6, "Messi")
+usuario6 = (6, "Ramon")
+usuario7 = (7, "Jorge")
+usuario8 = (8, "Silvina")
+usuario9 = (9, "Agustin")
+usuario10 = (10, "Colo")
+usuario11= (11, "Martin")
 
 
 relacion1_2 = (usuario1, usuario2)
@@ -108,8 +105,13 @@ relacion3_4 = (usuario4, usuario3)
 relacion4_5 = (usuario3, usuario5)
 relacion2_5 = (usuario2, usuario5)
 
+relacion2_6 = (usuario2,usuario6)
+relacion2_7 = (usuario2,usuario7)
+relacion2_8 = (usuario2,usuario8)
+relacion2_9 = (usuario2,usuario9)
+relacion2_10 = (usuario2,usuario10)
 
-
+relacion2_11 = (usuario2,usuario11)
 
 publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4, usuario5])
 publicacion1_2 = (usuario1, "No sé que postear", [usuario4])
@@ -117,11 +119,11 @@ publicacion1_3 = (usuario1, "Trabajo de haskell", [usuario2, usuario5])
 publicacion1_4 = (usuario1, "Primer cuatri", [])
 
 publicacion2_1 = (usuario2, "Hello World", [usuario4])
-publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario3, usuario2])
+publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8, usuario9, usuario10,usuario11])
 
 publicacion3_1 = (usuario3, "No", [])
 publicacion3_2 = (usuario3, "Si", [usuario2])
-publicacion3_3 = (usuario3, "Mensaje Subliminal", [usuario1, usuario5])
+publicacion3_3 = (usuario3, "Inserte frase motivacional", [usuario1, usuario5])
 
 publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
 publicacion4_2 = (usuario4, "I am Bob", [])
@@ -132,15 +134,21 @@ publicacion5_2 = (usuario5, "Qué tal", [usuario2])
 publicacion5_3 = (usuario5, "Todo bien", [usuario2, usuario5])
 
 
-usuariosPrimerRed = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6]
-relacionesPrimerRed = [relacion1_2, relacion1_3, relacion1_4, relacion2_3, relacion2_4, relacion3_4, relacion2_5]
+usuariosPrimerRed = [usuario1, usuario2, usuario3, usuario4, usuario5, usuario6, usuario7, usuario8, usuario9, usuario10]
+relacionesPrimerRed = [relacion1_2, relacion1_3, relacion1_4, relacion2_3, relacion2_4, relacion3_4, relacion2_5, relacion2_6, relacion2_7, relacion2_8, relacion2_8, relacion2_10, relacion2_11]
 publicacionesPrimerRed = [publicacion1_1, publicacion1_2, publicacion1_3, publicacion1_4, publicacion2_1, publicacion2_2,
                           publicacion3_1, publicacion3_2, publicacion3_3, publicacion4_1, publicacion4_2,
                           publicacion4_3, publicacion5_1, publicacion5_2, publicacion5_3]
 redUno = (usuariosPrimerRed, relacionesPrimerRed, publicacionesPrimerRed)
 
 
-usuariosSegundaRed = [usuario1, usuario2, usuario3]
+usuariosSegundaRed = [usuario1, usuario2, usuario3, usuario7]
 relacionesSegundaRed = [relacion1_2, relacion2_3, relacion1_3]
-publicacionesSegundaRed = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2]
+publicacionesSegundaRed = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion3_1, publicacion3_2]
 redDos = (usuariosSegundaRed, relacionesSegundaRed, publicacionesSegundaRed)
+
+usuariosRedTres = []
+relacionesRedTres = []
+publicacionesRedTres = []
+
+redTres = (usuariosRedTres,relacionesRedTres,publicacionesRedTres)
